91d6d5988b921c8a2385a5979821989b3bcbdca7
rajkumarkumawat <66380280+Rajkumar-kumawat@users.noreply.github.com>
2021-04-22 13:01:15 +0530

    Update footer.component.ts

--- frontend/src/app/@theme/components/footer/footer.component.ts
+++ frontend/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by" style="color: #05fcfc; font-family: Exo; font-size: 1.125rem; font-weight: 400;">I Know You - 2020</span>
+    <span class="created-by" style="color: #05fcfc; font-family: Exo; font-size: 1.125rem; font-weight: 400;">Swami-RAj-1.0</span>
     <div class="socials">
       <a href="https://twitter.com/Rjkumar_kumawat" target="_blank" class="ion ion-social-twitter"></a>
     </div>
